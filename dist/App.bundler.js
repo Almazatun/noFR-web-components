9c8ce5d9c32ed795f5d6d3dde51fe6b2e504418a
Almaz <atunstyle@gmail.com>
2021-03-09 11:32:16 +0300

    dist folder * Included dist folder files.

---
+++ dist/App.bundler.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+console.log("Hello");
+document.title = "Goodbye World!";
+/******/ })()
+;
+//# sourceMappingURL=App.bundler.js.map
